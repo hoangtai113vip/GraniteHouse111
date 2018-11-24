73c8715dc9b36c15c1e8684506bd127b6b8aa2ea
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-11-24 17:57:07 +0700

    add view create product types

--- GraniteHouse/Startup.cs
+++ GraniteHouse/Startup.cs
@@ -67,7 +67,7 @@ namespace GraniteHouse
             {
                 routes.MapRoute(
                   name: "areas",
-                  template: "{area=Admin}/{controller=Home}/{action=Index}/{id?}"
+                  template: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
 
