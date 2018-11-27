552580aea3dc18b6c992d20f11048a3aa5144a2d
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-11-27 20:08:22 +0700

    create action method product

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using GraniteHouse.Data;
 using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,12 +15,15 @@ namespace GraniteHouse.Controllers
     public class ProductsController : Controller
     {
         private ApplicationDbContext _db;
+        private HostingEnvironment _hostingEnvironment;
+
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
 
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
             ProductsVM = new ProductsViewModel()
             {
                 ProductTypes = _db.ProductTypes.ToList(),
@@ -33,6 +37,26 @@ namespace GraniteHouse.Controllers
         {
             var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
             return View(await products.ToListAsync());
+        }
+        //Get Create
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+        //post create
+        [HttpPost]
+        [ValidateAntiForgeryToken]  
+        public async Task<IActionResult> Create()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+            // image being saved
+            string webRootPath = _hostingEnvironment.WebRootPath;
+
         }
     }
 }
---
+++ GraniteHouse/Unitily/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Unitily
+{
+    public class SD
+    {
+        public const string DefaultProductImage = "default_product.jpg";
+        public string ImageFolder = @"images\ProductImage";
+    }
+}
