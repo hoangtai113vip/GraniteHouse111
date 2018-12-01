345de0f0aac427a3e0f68ee2f767cb2a5554f15e
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-12-01 20:35:31 +0700

    Details Page Post Action Method

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using GraniteHouse.Models;
 using GraniteHouse.Data;
 using Microsoft.EntityFrameworkCore;
+using GraniteHouse.Extensions;
 
 namespace GraniteHouse.Controllers
 {
@@ -28,6 +29,7 @@ namespace GraniteHouse.Controllers
              
             return View(await productList.ToListAsync());
         }
+        //Get details
         public async Task<IActionResult> Details(int id)
         {
             {
@@ -36,6 +38,22 @@ namespace GraniteHouse.Controllers
             }
         }
 
+        //Post Details action
+        [HttpPost,ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DetailsPost(int id)
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if(lstShoppingCart==null)
+            {
+                lstShoppingCart = new List<int>();
+            }
+            lstShoppingCart.Add(id);
+            HttpContext.Session.Set("ssShoppingCart",lstShoppingCart);
+            return RedirectToAction("Index", "Home",new { area="Customer"});
+
+
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
