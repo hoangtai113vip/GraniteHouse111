72cd9cf4d1804e1c1b6ecbe875c3ad4da0dd0969
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-12-09 10:11:40 +0700

    Add Sales Person To Appointment

---
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Unitily;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.AdminEndUser+","+SD.SuperAdminEndUser)]
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
